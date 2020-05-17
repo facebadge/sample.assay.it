31cf667ed0b9deeee322c116dd68d3cd3676690b
facebadge <facebadge@mobilana.mobi>
2020-05-17 10:15:32 +0300

    added suite

---
+++ suite.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/fogfish/gurl"
+	ƒ "github.com/fogfish/gurl/http/recv"
+	ø "github.com/fogfish/gurl/http/send"
+)
+
+// TestOk successful
+func TestOk() gurl.Arrow {
+	return gurl.HTTP(
+		ø.GET("https://example.com"),
+		ƒ.Code(200),
+	)
+}
+
+//
+func main() {}
