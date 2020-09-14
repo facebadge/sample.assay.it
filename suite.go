6b2e4cec55bc84792b2ce4c0faecb4d4acf95238
facebadge <facebadge@mobilana.mobi>
2020-09-14 22:45:40 +0300

    update the doc

--- suite.go
+++ suite.go
@@ -1,23 +1,30 @@
 /*
 
-Here is an example suite that illustrates an ability to apply contract testing of
-quality assessment strategy. The contact implements a test cases as function of the form:
+Here is an example of Behavior as a Code suite for testing of Serverless,
+Microservices and other systems that rely on interface syntaxes and its
+behaviors. The behavior is implemented as function of the form:
 
-  func TestAbc() gurl.Arrow
+  func TestAbc() assay.Arrow
 
 where Abc is a unique name of test case. Each case declares cause-and-effect:
 
-↣ "Given" specifies the communication context and the known state of the expected behavior;
+↣ "Given" specifies the communication context and the known state of the
+expected behavior;
 
 ↣ "When" executes key actions about the interaction with remote component;
 
-↣ "Then" observes output of remote component, validates its correctness and outputs results.
+↣ "Then" observes output of remote component, validates its correctness and
+outputs results.
 
-The service evaluates suites and its test cases sequentially one after another.
+assay.it evaluates suites and its functions sequentially one after another.
 
 */
 
-// Package sample is a standard Golang declaration. It groups set of logically related contracts.
+/*
+
+Package sample is a standard Golang declaration. It groups set of logically
+related contracts.
+*/
 package sample
 
 /*
@@ -25,51 +32,49 @@ package sample
 Standard Golang import declaration.
 
 However, assay.it restricts usage of some package.
-Please check doc.assay.it for details of allowed packages.
+Please check assay.it/doc/core for details of allowed packages.
 We are constantly looking for your feedback, please open an issue to us.
 */
 import (
 
 	//
-	// gurl library is a class of High Order Component which can do http requests
-	// with few interesting property such as composition and laziness.
-	// It implements a human-friendly syntax of HTTP communication and
-	// Behavior as a Code paradigm. It connect cause-and-effect with the networking
-	// primitives. Usage of gurl is a preferred approach for networking I/O.
-	"github.com/fogfish/gurl"
-	ƒ "github.com/fogfish/gurl/http/recv"
-	ø "github.com/fogfish/gurl/http/send"
+	// assay-it/sdk-go introduces pure functional and typesafe syntax to implement
+	// HTTP communication and Behavior as a Code suites.
+	"github.com/assay-it/sdk-go/assay"
+	"github.com/assay-it/sdk-go/http"
+	ƒ "github.com/assay-it/sdk-go/http/recv"
+	ø "github.com/assay-it/sdk-go/http/send"
 )
 
 /*
 
-TestOk makes HTTP request and checks the quality of the response.
-This is simple extample just validates that service is alive and
-responding with HTML document.
+TestOk makes HTTP request and checks the quality of the response. This is simple
+extample just validates that service is alive and responding with HTML document.
 
-Let's look in depth on the anatomy of the contract
+Let's look in depth on the anatomy
 */
-func TestOk() gurl.Arrow {
+func TestOk() assay.Arrow {
 	/*
-		The case is developed with Go URL library (gurl). This library defines
-		a rich techniques to hide the networking complexity using higher-order-functions
-		and its compositions. See https://assay.it/doc/core for details about api
+		SDK defines a rich techniques to hide the networking complexity using
+		higher-order-functions and its compositions. See https://assay.it/doc/core
+		for details about api.
 
-		gurl.HTTP lifts primitive protocol functions to higher-order suite.
+		http.Join lifts primitive protocol "modifiers" to higher-order suite.
 	*/
-	return gurl.HTTP(
-		// module ø (gurl/http/send) defines function to declare HTTP request.
+	return http.Join(
+		// module ø (http/send) defines function to declare HTTP request.
 		// See the https://assay.it/doc/core for details about module ø.
 
 		// declares HTTP method and destination URL
 		ø.GET("https://assay.it"),
 
-		// module ƒ (gurl/http/recv) defines function to validate correctness of HTTP protocol.
-		// Each ƒ constrain might terminate execution of consequent ƒ's if it expectation fails.
-		// See the https://assay.it/doc/core for details about module ƒ.
+		// module ƒ (http/recv) defines function to validate correctness of HTTP
+		// response. Each ƒ constrain might terminate execution of consequent ƒ's
+		// if it expectation fails. See the https://assay.it/doc/core for details
+		// about module ƒ.
 
 		// requires HTTP Status Code to be 200 OK
-		ƒ.Code(gurl.StatusCodeOK),
+		ƒ.Code(http.StatusCodeOK),
 
 		// requires response content-type to be text/html
 		ƒ.Header("Content-Type").Is("text/html"),
